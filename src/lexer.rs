042a099b3ad3b70c2f3c9c1d404ebc0aefd6de63
Gavin Morrow <gavinfmorrow@gmail.com>
2024-11-26 18:09:29 -0500

    Create lexer.rs

---
+++ src/lexer.rs
@@ -0,0 +1,10 @@
+pub fn lex(source: String) -> Vec<Token> {
+    todo!()
+}
+
+struct Token {
+    data: TokenData,
+    pos: usize,
+}
+
+enum TokenData {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod lexer;
 mod stream;
 
 fn main() {
