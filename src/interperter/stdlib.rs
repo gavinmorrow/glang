7687581ef8bfd9bc43cd046ed9e2e660d72398a2
Gavin Morrow <gavinfmorrow@gmail.com>
2024-12-01 09:14:12 -0500

    Use macro for defining stdlib stuff

--- src/interperter/stdlib.rs
+++ src/interperter/stdlib.rs
@@ -20,53 +20,52 @@ impl Environment {
 }
 
 fn define_stdlib(env: &mut Environment, scope: Scope) {
-    PrintFunc::define(env, scope.clone());
-    InputFunc::define(env, scope.clone());
+    macro_rules! native_func {
+        ($struct_name:ident, $func_def:ident) => {
+            #[derive(Debug)]
+            struct $struct_name;
+            impl $struct_name {
+                fn define(env: &mut Environment, scope: Scope) {
+                    let identifier = Identifier::new(scope, stringify!($func_def).to_string());
+                    env.define(identifier, Value::Func(Func::Native(&$struct_name)));
+                }
+            }
+            impl NativeFunc for $struct_name {
+                fn call(&self, arguments: Vec<Value>) -> super::Result<Value> {
+                    $func_def(arguments)
+                }
+            }
+            $struct_name::define(env, scope.clone());
+        };
+    }
+
+    native_func!(PrintFunc, print);
+    native_func!(InputFunc, input);
 }
 
-#[derive(Debug)]
-struct PrintFunc;
-impl PrintFunc {
-    fn define(env: &mut Environment, scope: Scope) {
-        let identifier = Identifier::new(scope, "print".to_string());
-        env.define(identifier, Value::Func(Func::Native(&PrintFunc)));
+fn print(arguments: Vec<Value>) -> super::Result<Value> {
+    for arg in arguments {
+        println!("{arg:#?}");
     }
-}
-impl NativeFunc for PrintFunc {
-    fn call(&self, arguments: Vec<Value>) -> super::Result<Value> {
-        for arg in arguments {
-            println!("{arg:#?}");
-        }
-        Ok(Value::Void)
-    }
+    Ok(Value::Void)
 }
 
-#[derive(Debug)]
-struct InputFunc;
-impl InputFunc {
-    fn define(env: &mut Environment, scope: Scope) {
-        let identifier = Identifier::new(scope, "input".to_string());
-        env.define(identifier, Value::Func(Func::Native(&InputFunc)));
+fn input(arguments: Vec<Value>) -> super::Result<Value> {
+    if arguments.len() > 1 {
+        return Err(Error::new(ErrorKind::IncorrectArity {
+            given: arguments.len(),
+            correct: 1,
+        }));
     }
-}
-impl NativeFunc for InputFunc {
-    fn call(&self, arguments: Vec<Value>) -> super::Result<Value> {
-        if arguments.len() > 1 {
-            return Err(Error::new(ErrorKind::IncorrectArity {
-                given: arguments.len(),
-                correct: 1,
-            }));
-        }
-        let empty_string = Value::Str("".to_string());
-        let question_str = arguments.first().unwrap_or(&empty_string);
-        print!("{question_str:?}");
-        std::io::stdout().flush().unwrap();
+    let empty_string = Value::Str("".to_string());
+    let question_str = arguments.first().unwrap_or(&empty_string);
+    print!("{question_str:?}");
+    std::io::stdout().flush().unwrap();
 
-        let mut input = String::new();
-        std::io::stdin()
-            .read_line(&mut input)
-            .map_err(|err| Error::new(ErrorKind::IOError(err)))?;
-        input.pop().expect("trailing newline should be removed");
-        Ok(Value::Str(input))
-    }
+    let mut input = String::new();
+    std::io::stdin()
+        .read_line(&mut input)
+        .map_err(|err| Error::new(ErrorKind::IOError(err)))?;
+    input.pop().expect("trailing newline should be removed");
+    Ok(Value::Str(input))
 }
