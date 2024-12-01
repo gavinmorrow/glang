dac6ae73631df3304fdbb4d29a4d0af44a0c9638
Gavin Morrow <gavinfmorrow@gmail.com>
2024-12-01 08:24:15 -0500

    Add function for reading a line of input

--- src/interperter/stdlib.rs
+++ src/interperter/stdlib.rs
@@ -1,6 +1,8 @@
+use std::io::Write;
+
 use super::{
     env::{Environment, Identifier, Scope},
-    Func, NativeFunc, Value,
+    Error, ErrorKind, Func, NativeFunc, Value,
 };
 
 impl Environment {
@@ -19,6 +21,7 @@ impl Environment {
 
 fn define_stdlib(env: &mut Environment, scope: Scope) {
     PrintFunc::define(env, scope.clone());
+    InputFunc::define(env, scope.clone());
 }
 
 #[derive(Debug)]
@@ -35,5 +38,35 @@ impl NativeFunc for PrintFunc {
             println!("{arg:#?}");
         }
         Ok(Value::Void)
+    }
+}
+
+#[derive(Debug)]
+struct InputFunc;
+impl InputFunc {
+    fn define(env: &mut Environment, scope: Scope) {
+        let identifier = Identifier::new(scope, "input".to_string());
+        env.define(identifier, Value::Func(Func::Native(&InputFunc)));
+    }
+}
+impl NativeFunc for InputFunc {
+    fn call(&self, arguments: Vec<Value>) -> super::Result<Value> {
+        if arguments.len() > 1 {
+            return Err(Error::new(ErrorKind::IncorrectArity {
+                given: arguments.len(),
+                correct: 1,
+            }));
+        }
+        let empty_string = Value::Str("".to_string());
+        let question_str = arguments.first().unwrap_or(&empty_string);
+        print!("{question_str:?}");
+        std::io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        std::io::stdin()
+            .read_line(&mut input)
+            .map_err(|err| Error::new(ErrorKind::IOError(err)))?;
+        input.pop().expect("trailing newline should be removed");
+        Ok(Value::Str(input))
     }
 }
