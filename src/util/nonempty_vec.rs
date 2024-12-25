c743855e4ea099c3aaa42356c1ba979fdb00b88e
Gavin Morrow <gavinfmorrow@gmail.com>
2024-12-25 13:51:23 -0500

    Remove unused nonempty stuff

--- src/util/nonempty_vec.rs
+++ src/util/nonempty_vec.rs
@@ -5,24 +5,12 @@ use std::ops::{Deref, Index, IndexMut};
 pub struct NEVec<T>(Vec<T>);
 
 impl<T> NEVec<T> {
-    pub fn new(values: Vec<T>) -> Option<Self> {
-        if values.is_empty() {
-            None
-        } else {
-            Some(NEVec(values))
-        }
-    }
-
     pub fn single(value: T) -> Self {
         NEVec(vec![value])
     }
 }
 
 impl<T> NEVec<T> {
-    pub fn last(&self) -> &T {
-        self.0.last().unwrap()
-    }
-
     pub fn last_mut(&mut self) -> &mut T {
         self.0.last_mut().unwrap()
     }
