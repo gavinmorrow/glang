58441f7157898cadb9ef5b0ad1c8a8f57bf28b49
Gavin Morrow <gavinfmorrow@gmail.com>
2025-01-26 20:25:59 -0500

    Remove a bunch of useless fields from WasmGen

--- src/wasm_backend.rs
+++ src/wasm_backend.rs
@@ -16,88 +16,55 @@ use crate::{
 
 pub fn gen_wasm(program: Program) -> Module {
     let mut wasm_gen = WasmGen::new(program);
-    wasm_gen.gen();
-    wasm_gen.finish()
+    wasm_gen.gen()
 }
 
 struct WasmGen {
     program: Program,
-
-    module: Module,
-
-    type_section: TypeSection,
-    import_section: ImportSection,
-    function_section: FunctionSection,
-    table_section: TableSection,
-    memory_section: MemorySection,
-    global_section: GlobalSection,
-    export_section: ExportSection,
-    start_section: StartSection,
-    element_section: ElementSection,
-    code_section: CodeSection,
-    data_section: DataSection,
-    data_count_section: DataCountSection,
 }
 
 impl WasmGen {
     fn new(program: Program) -> Self {
-        WasmGen {
-            program,
-
-            module: Module::new(),
-
-            type_section: TypeSection::new(),
-            import_section: ImportSection::new(),
-            function_section: FunctionSection::new(),
-            table_section: TableSection::new(),
-            memory_section: MemorySection::new(),
-            global_section: GlobalSection::new(),
-            export_section: ExportSection::new(),
-            start_section: StartSection { function_index: 0 },
-            element_section: ElementSection::new(),
-            code_section: CodeSection::new(),
-            data_section: DataSection::new(),
-            data_count_section: DataCountSection { count: 0 },
-        }
+        WasmGen { program }
     }
 
-    fn finish(mut self) -> Module {
-        self.module.section(&self.type_section);
-        self.module.section(&self.import_section);
-        self.module.section(&self.function_section);
-        self.module.section(&self.table_section);
-        self.module.section(&self.memory_section);
-        self.module.section(&self.global_section);
-        self.module.section(&self.export_section);
-        self.module.section(&self.start_section);
-        self.module.section(&self.element_section);
-        self.module.section(&self.data_count_section);
-        self.module.section(&self.code_section);
-        self.module.section(&self.data_section);
+    fn finish(mut self, main: &Function) -> Module {
+        let mut type_section = TypeSection::new();
+        let mut import_section = ImportSection::new();
+        let mut function_section = FunctionSection::new();
+        let mut code_section = CodeSection::new();
+        let mut start_section = StartSection { function_index: 1 };
 
-        self.module
-    }
-
-    fn gen(&mut self) {
         // Set up imports
-        self.type_section.ty().function([ValType::F64], []);
-        self.import_section
-            .import("std", "print", EntityType::Function(0));
+        type_section.ty().function([ValType::F64], []);
+        import_section.import("std", "print", EntityType::Function(0));
 
         // Set up main function
-        self.type_section.ty().function([], []);
-        self.function_section.function(1);
-        self.start_section.function_index = 1;
+        type_section.ty().function([], []);
+        function_section.function(1);
+        start_section.function_index = 1;
+        code_section.function(main);
 
+        let mut module = Module::new();
+        module
+            .section(&type_section)
+            .section(&import_section)
+            .section(&function_section)
+            .section(&code_section)
+            .section(&start_section);
+        module
+    }
+
+    fn gen(mut self) -> Module {
         let locals = [];
-
         let mut main = Function::new(locals);
+
         for stmt in self.program.clone() {
             self.gen_stmt(&mut main, &stmt);
         }
         main.instruction(&wasm_encoder::Instruction::End);
 
-        self.code_section.function(&main);
+        self.finish(&main)
     }
 
     fn gen_stmt(&mut self, func: &mut wasm_encoder::Function, stmt: &Stmt) {
